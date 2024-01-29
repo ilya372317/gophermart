6e1bb372bfffd712757cb41867b710e35fc47d5d
Ilya Otinov <ilya.otinov@gmail.com>
2024-01-29 13:11:26 +0700

    fix: json response make correct

--- internal/gmiddleware/jsonres.go
+++ internal/gmiddleware/jsonres.go
@@ -5,7 +5,7 @@ import "net/http"
 func JSONResponse() func(h http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-			request.Header.Set("Content-Type", "application/json")
+			writer.Header().Set("Content-Type", "application/json")
 			h.ServeHTTP(writer, request)
 		})
 	}
